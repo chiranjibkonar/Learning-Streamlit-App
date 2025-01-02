6cefd0d815def1f424317130bec5732aceba4b29
chiranjibkonar <chiranjib.konar@gmail.com>
2025-01-02 11:53:10 +0530

    Update learning-streamlit.py

--- learning-streamlit.py
+++ learning-streamlit.py
@@ -43,3 +43,8 @@ if R:
   st.write(area(R))
 st.latex(r" \alpha + \beta " )
 st.write(r"$ \frac{\alpha + \beta}{\gamma + \delta} $" )
+col1, col2=st.columns(2)
+with col1:
+  st.write("col1") 
+with col2:
+  st.write("col2")
