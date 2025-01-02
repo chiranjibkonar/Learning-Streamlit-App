09c5a86555e2cc2a3c48d0ff9e3ebd016e5f8d8e
chiranjibkonar <chiranjib.konar@gmail.com>
2025-01-02 11:40:55 +0530

    Update learning-streamlit.py

--- learning-streamlit.py
+++ learning-streamlit.py
@@ -8,7 +8,7 @@ st.title("Nothing")
 st.header("Header")
 st.markdown("markdown")
 st.markdown("## markdown") 
-"For Colours"
+st.header("For Colours")
 st.error("Error") 
 st.warning("warning") 
 st.info("Info") 
