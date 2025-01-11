f8c15dd19e12f074730db6b2b3bc08e9c09dbb6d
chiranjibkonar <chiranjib.konar@gmail.com>
2025-01-11 12:37:47 +0530

    Update learning-graphs.py

--- learning-graphs.py
+++ learning-graphs.py
@@ -1,7 +1,7 @@
 # learning graphs
 import numpy as np 
 import streamlit as st 
-import matplotlib.pyplot as plt 
+import matplotlib
 
 x=np.linspace(10,100,10)  # Explanation: Along x axis: 1st value = start value, 2nd value = end value, 3rd value is the number of points 
 y=[i**2 for i in x] 
