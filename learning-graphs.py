1a063de12852bb916b97109cc2241fb6c49f5e0b
pranjalsh22 <physics0403@gmail.com>
2025-03-05 10:34:37 +0530

    Update learning-graphs.py

--- learning-graphs.py
+++ learning-graphs.py
@@ -3,13 +3,13 @@
 import matplotlib.pyplot as plt
 import numpy as np 
 import streamlit as st 
-"hi"
+
 x=np.linspace(10,100,10)  # Explanation: Along x axis: 1st value = start value, 2nd value = end value, 3rd value is the number of points 
 y=[i**2 for i in x] 
 st.write(f"x values are: {x}")   # f means formatted string, 
 st.write(f"yvalues are: {y}")    
 plt.figure()
 plt.plot(x,y)
-
+st.pyplot(fig)
 
 
